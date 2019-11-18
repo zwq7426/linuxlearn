a166939a5689b4fabef9df9e3a27a6ec513247e5
zwq7426 <zwq8714@163.com>
2019-11-17 16:08:43 -0800

    week 10

---
+++ week10/code/onclass/1-4.c
@@ -0,0 +1,31 @@
+#include"my.h"
+static void callback1(){
+	printf("--------------callback1-------------\n");
+}
+static void callback2(){
+	printf("--------------callback2-------------\n");
+}
+static void __attribute__((constructor)) before_main(){
+	printf("--------------constructor-------------\n");
+}
+static void __attribute__((destructor)) after_main(){
+	printf("--------------destructor-------------\n");
+}
+int main()
+{
+	atexit(callback1);
+	atexit(callback2);
+	FILE *fp;
+	char buf[]={"test data from fill buffer!\n"};
+	if((fp=fopen("test.dat","w+"))==NULL){
+		perror("failed to fopen\n");
+		return -1;	
+	}
+	if(fputs(buf,fp)==EOF){
+		perror("fail to fput!\n");
+	}
+	printf("printf:data from line buffer\n");
+	//exit(0);
+	//_exit(0);
+	return 0;
+}
---
+++ week10/code/onclass/1-5.c
@@ -0,0 +1,8 @@
+#include"my.h"
+int g=10;
+int main(){
+	pid_t pid;
+	pid=fork();
+	if(pid<0)
+
+}
