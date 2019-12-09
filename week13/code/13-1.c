cd274f66838b32251ffc2ac470140c09b48b4672
zwq7426 <zwq8714@163.com>
2019-12-08 16:12:24 -0800

    week13

---
+++ week13/code/13-1.c
@@ -0,0 +1,22 @@
+#include"my.h"
+
+void sigtoutinne(int dunno){
+	switch(dunno){
+		case 1:
+			printf("Get a signal SIGHUP\n");		
+			break;
+		case 2:
+			printf("Get a signal SIGHUP\n");
+			break;
+		case 3:
+			printf("Get a signal SIGHUP\n");
+			break;
+	}
+}
+int main(){
+	printf("process id is %d\n",getpid());
+	signal(SIGHUP,sigtoutine);
+	signal(SIGINT,sigtoutine);
+	signal(SIGQUIT,sigtoutine);
+	while(1);
+}
---
+++ week13/code/13-2.c
@@ -0,0 +1,19 @@
+#include"my.h"
+#define PPROMPT "Do you want terminate program?"
+char *prompt=PROMPT;
+void ctrl_c(signo){
+	fprintf(stderr,prompt);
+}
+
+int main{
+	struct sigaction act;
+	act.sa_handler=ctrl_c;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags=0;
+	if(sigaction(SIGINT,&act,NULL)<0){
+		printf(stderr,"Install Signal Action Error:%s\n",strerror(errno));
+		exit(1);
+	}
+	printf("process id is %	d\n"getpid());
+	while(1);
+}
